da66478aa0bf38a4b578160f8347bfbc5db063a5
Michael Quinn <mquinn5@binghamton.edu>
2020-12-26 16:29:51 -0500

    added 'Up' to drone docking, if available

--- AutoMiner.cs
+++ AutoMiner.cs
@@ -2289,6 +2289,11 @@ void SetGyroscopes(){
 		input_yaw+=(float)Math.Min(Math.Max(difference/5,-4),4)*gyro_multx;
 	
 	float input_roll=current_roll*0.99f;
+	if(MyDock!=null&&MyDock.DoUp&&Tasks.Peek()==DroneTask.Docking){
+		difference=GetAngle(Left_Vector,MyDock.Up)-GetAngle(Right_Vector,MyDock.Up);
+		if(Math.Abs(difference)>.1)
+			input_roll-=(float)Math.Min(Math.Max(difference/5,-4),4)*gyro_multx;
+	}
 	
 	Vector3D input=new Vector3D(input_pitch,input_yaw,input_roll);
 	
