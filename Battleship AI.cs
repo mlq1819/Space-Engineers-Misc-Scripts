be5f0cf5c572250483cf528591a85024c9da3d71
Michael Quinn <mquinn5@binghamton.edu>
2021-01-03 16:49:58 -0500

    started work on Battleship game Ship AI

---
+++ Battleship AI.cs
@@ -0,0 +1,439 @@
+const string Program_Name = ""; //Name me!
+Color DEFAULT_TEXT_COLOR=new Color(197,137,255,255);
+Color DEFAULT_BACKGROUND_COLOR=new Color(44,0,88,255);
+
+class Prog{
+	public static MyGridProgram P;
+}
+
+class GenericMethods<T> where T : class, IMyTerminalBlock{
+	static IMyGridTerminalSystem TerminalSystem{
+		get{
+			return P.GridTerminalSystem;
+		}
+	}
+	public static MyGridProgram P{
+		get{
+			return Prog.P;
+		}
+	}
+	
+	public static T GetFull(string name,Vector3D Ref,double mx_d=double.MaxValue){
+		List<T> AllBlocks=new List<T>();
+		List<T> MyBlocks=new List<T>();
+		TerminalSystem.GetBlocksOfType<T>(AllBlocks);
+		double min_distance=mx_d;
+		foreach(T Block in AllBlocks){
+			if(Block.CustomName.Equals(name)){
+				double distance=(Ref-Block.GetPosition()).Length();
+				min_distance=Math.Min(min_distance, distance);
+				MyBlocks.Add(Block);
+			}
+		}
+		foreach(T Block in MyBlocks){
+			double distance=(Ref-Block.GetPosition()).Length();
+			if(distance<=min_distance+0.1)
+				return Block;
+		}
+		return null;
+	}
+	
+	public static T GetFull(string name,IMyTerminalBlock Ref,double mx_d=double.MaxValue){
+		return GetFull(name,Ref.GetPosition(),mx_d);
+	}
+	
+	public static T GetFull(string name,double mx_d=double.MaxValue){
+		return GetFull(name,P.Me,mx_d);
+	}
+	
+	public static T GetConstruct(string name,IMyTerminalBlock Ref,double mx_d=double.MaxValue){
+		List<T> input=GetAllConstruct(name,Ref,mx_d);
+		if(input.Count>0)
+			return input[0];
+		return null;
+	}
+	
+	public static T GetConstruct(string name,double mx_d=double.MaxValue){
+		return GetConstruct(name,P.Me,mx_d);
+	}
+	
+	public static List<T> GetAllConstruct(string name,IMyTerminalBlock Ref,double mx_d=double.MaxValue){
+		List<T> input=GetAllContaining(name,Ref,mx_d);
+		List<T> output=new List<T>();
+		foreach(T Block in input){
+			if(Ref.IsSameConstructAs(Block))
+				output.Add(Block);
+		}
+		return output;
+	}
+	
+	public static List<T> GetAllConstruct(string name){
+		return GetAllConstruct(name,P.Me);
+	}
+	
+	public static T GetContaining(string name,Vector3D Ref,double mx_d){
+		List<T> AllBlocks=new List<T>();
+		List<T> MyBlocks=new List<T>();
+		TerminalSystem.GetBlocksOfType<T>(AllBlocks);
+		double min_distance=mx_d;
+		foreach(T Block in AllBlocks){
+			if(Block.CustomName.Contains(name)){
+				double distance=(Ref-Block.GetPosition()).Length();
+				min_distance=Math.Min(min_distance,distance);
+				MyBlocks.Add(Block);
+			}
+		}
+		foreach(T Block in MyBlocks){
+			double distance=(Ref-Block.GetPosition()).Length();
+			if(distance<=min_distance+0.1)
+				return Block;
+		}
+		return null;
+	}
+	
+	public static T GetContaining(string name,IMyTerminalBlock Ref,double mx_d){
+		return GetContaining(name,Ref.GetPosition(),mx_d);
+	}
+	
+	public static T GetContaining(string name,double mx_d=double.MaxValue){
+		return GetContaining(name,P.Me,mx_d);
+	}
+	
+	public static List<T> GetAllContaining(string name,Vector3D Ref,double mx_d){
+		List<T> AllBlocks=new List<T>();
+		List<List<T>> MyLists=new List<List<T>>();
+		List<T> MyBlocks=new List<T>();
+		TerminalSystem.GetBlocksOfType<T>(AllBlocks);
+		foreach(T Block in AllBlocks){
+			if(Block.CustomName.Contains(name)){
+				bool has_with_name=false;
+				for(int i=0;i<MyLists.Count&&!has_with_name;i++){
+					if(Block.CustomName.Equals(MyLists[i][0].CustomName)){
+						MyLists[i].Add(Block);
+						has_with_name=true;
+						break;
+					}
+				}
+				if(!has_with_name){
+					List<T> new_list=new List<T>();
+					new_list.Add(Block);
+					MyLists.Add(new_list);
+				}
+			}
+		}
+		foreach(List<T> list in MyLists){
+			if(list.Count==1){
+				MyBlocks.Add(list[0]);
+				continue;
+			}
+			double min_distance=mx_d;
+			foreach(T Block in list){
+				double distance=(Ref-Block.GetPosition()).Length();
+				min_distance=Math.Min(min_distance, distance);
+			}
+			foreach(T Block in list){
+				double distance=(Ref-Block.GetPosition()).Length();
+				if(distance<=min_distance+0.1){
+					MyBlocks.Add(Block);
+					break;
+				}
+			}
+		}
+		return MyBlocks;
+	}
+	
+	public static List<T> GetAllIncluding(string name,Vector3D Ref,double mx_d=double.MaxValue){
+		List<T> AllBlocks=new List<T>();
+		List<T> MyBlocks=new List<T>();
+		TerminalSystem.GetBlocksOfType<T>(AllBlocks);
+		foreach(T Block in AllBlocks){
+			double distance=(Ref-Block.GetPosition()).Length();
+			if(Block.CustomName.Contains(name)&&distance<=mx_d)
+				MyBlocks.Add(Block);
+		}
+		return MyBlocks;
+	}
+	
+	public static List<T> GetAllIncluding(string name,IMyTerminalBlock Ref,double mx_d=double.MaxValue){
+		return GetAllIncluding(name,Ref.GetPosition(),mx_d);
+	}
+	
+	public static List<T> GetAllIncluding(string name,double mx_d=double.MaxValue){
+		return GetAllIncluding(name,P.Me,mx_d);
+	}
+	
+	public static List<T> GetAllContaining(string name,IMyTerminalBlock Ref,double mx_d=double.MaxValue){
+		return GetAllContaining(name,Ref.GetPosition(),mx_d);
+	}
+	
+	public static List<T> GetAllContaining(string name,double mx_d=double.MaxValue){
+		return GetAllContaining(name,P.Me,mx_d);
+	}
+	
+	public static List<T> GetAllFunc(Func<T,bool> f){
+		List<T> AllBlocks=new List<T>();
+		List<T> MyBlocks=new List<T>();
+		TerminalSystem.GetBlocksOfType<T>(AllBlocks);
+		foreach(T Block in AllBlocks){
+			if(f(Block))
+				MyBlocks.Add(Block);
+		}
+		return MyBlocks;
+	}
+	
+	public static T GetClosestFunc(Func<T,bool> f,Vector3D Ref,double mx_d=double.MaxValue){
+		List<T> MyBlocks=GetAllFunc(f);
+		double min_distance=mx_d;
+		foreach(T Block in MyBlocks){
+			double distance=(Ref-Block.GetPosition()).Length();
+			min_distance=Math.Min(min_distance,distance);
+		}
+		foreach(T Block in MyBlocks){
+			double distance=(Ref-Block.GetPosition()).Length();
+			if(distance<=min_distance+0.1)
+				return Block;
+		}
+		return null;
+	}
+	
+	public static T GetClosestFunc(Func<T,bool> f,IMyTerminalBlock Ref,double mx_d=double.MaxValue){
+		return GetClosestFunc(f,Ref.GetPosition(),mx_d);
+	}
+	
+	public static T GetClosestFunc(Func<T,bool> f,double mx_d=double.MaxValue){
+		return GetClosestFunc(f,P.Me,mx_d);
+	}
+	
+	public static T GetGrid(string name,IMyCubeGrid Grid,IMyTerminalBlock Ref,double mx_d=double.MaxValue){
+		List<T> input=GetAllGrid(name,Grid,Ref,mx_d);
+		if(input.Count>0)
+			return input[0];
+		return null;
+	}
+	
+	public static T GetGrid(string name,IMyCubeGrid Grid,double mx_d=double.MaxValue){
+		return GetGrid(name,Grid,P.Me,mx_d);
+	}
+	
+	public static List<T> GetAllGrid(string name,IMyCubeGrid Grid,IMyTerminalBlock Ref,double mx_d){
+		List<T> output=new List<T>();
+		List<T> input=GetAllContaining(name,Ref,mx_d);
+		foreach(T Block in input){
+			if(Block.CubeGrid==Grid)
+				output.Add(Block);
+		}
+		return output;
+	}
+	
+	public static List<T> GetAllGrid(string name,IMyCubeGrid Grid,double mx_d=double.MaxValue){
+		return GetAllGrid(name,Grid,P.Me,mx_d);
+	}
+	
+	public static List<T> SortByDistance(List<T> unsorted,Vector3D Ref){
+		List<T> output=new List<T>();
+		while(unsorted.Count>0){
+			double min_distance=double.MaxValue;
+			foreach(T Block in unsorted){
+				double distance=(Ref-Block.GetPosition()).Length();
+				min_distance=Math.Min(min_distance,distance);
+			}
+			for(int i=0; i<unsorted.Count; i++){
+				double distance=(Ref-unsorted[i].GetPosition()).Length();
+				if(distance<=min_distance+0.1){
+					output.Add(unsorted[i]);
+					unsorted.RemoveAt(i);
+					break;
+				}
+			}
+		}
+		return output;
+	}
+	
+	public static List<T> SortByDistance(List<T> unsorted,IMyTerminalBlock Ref){
+		return SortByDistance(unsorted, Ref.GetPosition());
+	}
+	
+	public static List<T> SortByDistance(List<T> unsorted){
+		return SortByDistance(unsorted,P.Me);
+	}
+	
+	public static double GetAngle(Vector3D v1, Vector3D v2){
+		v1.Normalize();
+		v2.Normalize();
+		return Math.Round(Math.Acos(v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z)*57.295755,5);
+	}
+}
+
+TimeSpan FromSeconds(double seconds){
+	return (new TimeSpan(0,0,0,(int)seconds,(int)(seconds*1000)%1000));
+}
+
+TimeSpan UpdateTimeSpan(TimeSpan old,double seconds){
+	return old+FromSeconds(seconds);
+}
+
+string ToString(TimeSpan ts){
+	if(ts.TotalDays>=1)
+		return Math.Round(ts.TotalDays,2).ToString()+" days";
+	else if(ts.TotalHours>=1)
+		return Math.Round(ts.TotalHours,2).ToString()+" hours";
+	else if(ts.TotalMinutes>=1)
+		return Math.Round(ts.TotalMinutes,2).ToString()+" minutes";
+	else if(ts.TotalSeconds>=1)
+		return Math.Round(ts.TotalSeconds,3).ToString()+" seconds";
+	else 
+		return Math.Round(ts.TotalMilliseconds,0).ToString()+" milliseconds";
+}
+
+bool HasBlockData(IMyTerminalBlock Block, string Name){
+	if(Name.Contains(':'))
+		return false;
+	string[] args=Block.CustomData.Split('•');
+	foreach(string argument in args){
+		if(argument.IndexOf(Name+':')==0){
+			return true;
+		}
+	}
+	return false;
+}
+
+string GetBlockData(IMyTerminalBlock Block, string Name){
+	if(Name.Contains(':'))
+		return "";
+	string[] args=Block.CustomData.Split('•');
+	foreach(string argument in args){
+		if(argument.IndexOf(Name+':')==0){
+			return argument.Substring((Name+':').Length);
+		}
+	}
+	return "";
+}
+
+bool SetBlockData(IMyTerminalBlock Block, string Name, string Data){
+	if(Name.Contains(':'))
+		return false;
+	string[] args=Block.CustomData.Split('•');
+	for(int i=0; i<args.Count(); i++){
+		if(args[i].IndexOf(Name+':')==0){
+			Block.CustomData=Name+':'+Data;
+			for(int j=0; j<args.Count(); j++){
+				if(j!=i){
+					Block.CustomData+='•'+args[j];
+				}
+			}
+			return true;
+		}
+	}
+	Block.CustomData+='•'+Name+':'+Data;
+	return true;
+}
+
+Vector3D GlobalToLocal(Vector3D Global,IMyCubeBlock Ref){
+	Vector3D Local=Vector3D.Transform(Global+Ref.GetPosition(), MatrixD.Invert(Ref.WorldMatrix));
+	Local.Normalize();
+	return Local*Global.Length();
+}
+
+Vector3D GlobalToLocalPosition(Vector3D Global,IMyCubeBlock Ref){
+	Vector3D Local=Vector3D.Transform(Global, MatrixD.Invert(Ref.WorldMatrix));
+	Local.Normalize();
+	return Local*(Global-Ref.GetPosition()).Length();
+}
+
+Vector3D LocalToGlobal(Vector3D Local,IMyCubeBlock Ref){
+	Vector3D Global=Vector3D.Transform(Local, Ref.WorldMatrix)-Ref.GetPosition();
+	Global.Normalize();
+	return Global*Local.Length();
+}
+
+Vector3D LocalToGlobalPosition(Vector3D Local,IMyCubeBlock Ref){
+	return Vector3D.Transform(Local,Ref.WorldMatrix);
+}
+
+double GetAngle(Vector3D v1,Vector3D v2){
+	return GenericMethods<IMyTerminalBlock>.GetAngle(v1,v2);
+}
+
+void Write(string text,bool new_line=true,bool append=true){
+	Echo(text);
+	if(new_line)
+		Me.GetSurface(0).WriteText(text+'\n', append);
+	else
+		Me.GetSurface(0).WriteText(text, append);
+}
+
+TimeSpan Time_Since_Start=new TimeSpan(0);
+long cycle=0;
+char loading_char='|';
+double seconds_since_last_update=0;
+
+public Program(){
+	Prog.P=this;
+	Me.CustomName=(Program_Name+" Programmable block").Trim();
+	for(int i=0;i<Me.SurfaceCount;i++){
+		Me.GetSurface(i).FontColor=DEFAULT_TEXT_COLOR;
+		Me.GetSurface(i).BackgroundColor=DEFAULT_BACKGROUND_COLOR;
+		Me.GetSurface(i).Alignment=TextAlignment.CENTER;
+		Me.GetSurface(i).ContentType=ContentType.TEXT_AND_IMAGE;
+	}
+	Me.GetSurface(1).FontSize=2.2f;
+	Me.GetSurface(1).TextPadding=40.0f;
+	Echo("Beginning initialization");
+	// The constructor, called only once every session and
+    // always before any other method is called. Use it to
+    // initialize your script. 
+    //     
+    // The constructor is optional and can be removed if not
+    // needed.
+    // 
+    // It's recommended to set RuntimeInfo.UpdateFrequency 
+    // here, which will allow your script to run itself without a 
+    // timer block.
+}
+
+public void Save(){
+    // Called when the program needs to save its state. Use
+    // this method to save your state to the Storage field
+    // or some other means. 
+    // 
+    // This method is optional and can be removed if not
+    // needed.
+}
+
+void UpdateProgramInfo(){
+	cycle=(++cycle)%long.MaxValue;
+	switch(loading_char){
+		case '|':
+			loading_char='\\';
+			break;
+		case '\\':
+			loading_char='-';
+			break;
+		case '-':
+			loading_char='/';
+			break;
+		case '/':
+			loading_char='|';
+			break;
+	}
+	Write("",false,false);
+	Echo(Program_Name+" OS Cycle-"+cycle.ToString()+" ("+loading_char+")");
+	Me.GetSurface(1).WriteText(Program_Name+" OS Cycle-"+cycle.ToString()+" ("+loading_char+")",false);
+	seconds_since_last_update=Runtime.TimeSinceLastRun.TotalSeconds + (Runtime.LastRunTimeMs / 1000);
+	Echo(ToString(FromSeconds(seconds_since_last_update))+" since last cycle");
+	Time_Since_Start=UpdateTimeSpan(Time_Since_Start,seconds_since_last_update);
+	Echo(ToString(Time_Since_Start)+" since last reboot\n");
+	Me.GetSurface(1).WriteText("\n"+ToString(Time_Since_Start)+" since last reboot",true);
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+	UpdateProgramInfo();
+    // The main entry point of the script, invoked every time
+    // one of the programmable block's Run actions are invoked,
+    // or the script updates itself. The updateSource argument
+    // describes where the update came from.
+    // 
+    // The method itself is required, but the arguments above
+    // can be removed if not needed.
+}
