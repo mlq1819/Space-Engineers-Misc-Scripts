e6c55298063bd73d07bc659c00b5ec53411307a2
Michael Quinn <mquinn5@binghamton.edu>
2021-01-05 21:11:02 -0500

    tweaked time on Antenna Timer

--- Battleship AI.cs
+++ Battleship AI.cs
@@ -1021,7 +1021,7 @@ void Return(){
 	Runtime.UpdateFrequency=UpdateFrequency.Update10;
 }
 
-double Antenna_Timer=5;
+double Antenna_Timer=7.5;
 public void Main(string argument, UpdateType updateSource)
 {
 	UpdateProgramInfo();
@@ -1061,10 +1061,10 @@ public void Main(string argument, UpdateType updateSource)
 		Detonate();
 	if(Status==ShipStatus.Returning)
 		Return();
-	if(Antenna_Timer<5)
+	if(Antenna_Timer<7.5)
 		Antenna_Timer+=seconds_since_last_update;
 	
-	if(Antenna_Timer>=5&&Antenna_L.Status!=MyLaserAntennaStatus.Connected&&Target_Laser.Length()!=0&&(Target_Laser-Controller.GetPosition()).Length()<5000){
+	if(Antenna_Timer>=7.5&&Antenna_L.Status!=MyLaserAntennaStatus.Connected&&Target_Laser.Length()!=0&&(Target_Laser-Controller.GetPosition()).Length()<5000){
 		if((Antenna_L.TargetCoords-Target_Laser).Length()>.5){
 			Antenna_L.SetTargetCoords((new MyWaypointInfo("Target!!!",Target_Laser)).ToString());
 			Antenna_L.Connect();
