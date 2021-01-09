fb4aa138c3f09951b6e434b6ffbdce279e2e97cf
Michael Quinn <mquinn5@binghamton.edu>
2021-01-09 17:21:49 -0500

    added a probability-based method for predicting ship locations for the hardest AI

--- Battleship HUB.cs
+++ Battleship HUB.cs
@@ -756,11 +756,9 @@ class Board{
 				}
 			}
 		}
-		for(int y=0;y<8;y++){
-			for(int x=0;x<8;x++){
+		for(int y=0;y<8;y++)
+			for(int x=0;x<8;x++)
 				output[y][x]/=(float)total;
-			}
-		}
 		return output;
 	}
 	public int ShipNumber=1;
