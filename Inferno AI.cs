af19d9b70eb348f191ae9ec179ebdbc126de3ef0
Michael Quinn <mquinn5@binghamton.edu>
2021-04-20 19:36:15 -0400

    slight tweaks to gyroscopic orbital corrections

--- Inferno AI.cs
+++ Inferno AI.cs
@@ -2053,15 +2053,27 @@ void SetGyroscopes(){
 		if(Safety){
 			if((((Elevation-MySize)<Controller.GetShipSpeed()*2&&(Elevation-MySize)<50)||(Controller.DampenersOverride&&!Controller.IsUnderControl)||Orbiting)&&GetAngle(Gravity,Forward_Vector)<120&&Pitch_Time>=1){
 				double difference=Math.Abs(GetAngle(Gravity,Forward_Vector));
-				if(Orbiting&&Orbital_Altitude-Sealevel>100)
-					difference=Math.Max(difference-15,0);
+				if(Orbiting){
+					if(Orbital_Altitude-Sealevel>100)
+						difference=Math.Max(difference-15,0);
+					else if(Orbital_Altitude-Sealevel>50)
+						difference=Math.Max(difference-10,0);
+					else if(Orbital_Altitude-Sealevel>25)
+						difference=Math.Max(difference-5,0);
+				}
 				if(difference<90)
 					input_pitch-=10*gyro_multx*((float)Math.Min(Math.Abs((90-difference)/90),1));
 			}
 			if((Orbiting||(Controller.DampenersOverride&&!Undercontrol))&&(GetAngle(Gravity,Forward_Vector)>(90+Acceptable_Angle/2)||(Orbiting&&GetAngle(Gravity,Forward_Vector)>60))){
 				double difference=Math.Abs(GetAngle(Gravity,Forward_Vector));
-				if(Orbiting&&Sealevel-Orbital_Altitude>100)
-					difference=Math.Min(difference+15,180);
+				if(Orbiting){
+					if(Sealevel-Orbital_Altitude>100)
+						difference=Math.Min(difference+15,180);
+					else if(Sealevel-Orbital_Altitude>50)
+						difference=Math.Min(difference+10,180);
+					else if(Sealevel-Orbital_Altitude>25)
+						difference=Math.Min(difference+5,180);
+				}
 				if(difference>90+Acceptable_Angle/2||(Orbiting&&difference>90))
 					input_pitch+=10*gyro_multx*((float)Math.Min(Math.Abs((difference-90)/90),1))*orbit_multx;
 			}
